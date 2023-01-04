a6c71fb2d8c1ddb9c62760f2d611c833292aeb2a
marta szuran <marta.szuran@dataart.com>
2023-01-04 10:29:31 +0100

    implementing patch and fix addNew taks logic

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,18 +5,30 @@ const randomFunction = require('../utils/randomFunction.js');
 
 const newTaskPrototype = {
     'id': randomFunction(),
-    'type': 'todo',
-  }
+    'type': 'toDo',
+};
 
+  
 router.post('/addNew', (req, res) => {
     const newTaskPayload = req.body.task;
     const newTask = Object.assign(newTaskPrototype, newTaskPayload);
     mockupTasks.push(newTask)
     res.send('success');
-})
-
+});
+    
+router.patch('/editTask', (req, res) => {
+    const newTask = req.body.task;
+    mockupTasks = mockupTasks.map(task => {
+        if (task.id === Number(newTask.id)) {
+            return newTask;
+        }
+        return task;
+    });
+    res.send('success');
+});
+    
 router.get('/', (req, res) => {
     res.json(mockupTasks);
-})
+});
 
 module.exports = router;
