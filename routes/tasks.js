db53f6b4e4525ca4d796354d184aa1ceebd8c85e
marta szuran <marta.szuran@dataart.com>
2023-01-04 21:21:56 +0100

    setting up server

---
+++ routes/tasks.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+let mockupTasks = require('../constants/mockupTasks');
+const generateId = require('../utils/generateId.js');
+
+router.post('/addNew', (req, res) => {
+    const newTaskPayload = req.body.task;
+    const newTask = {id: generateId(), ...newTaskPayload, type: 'toDo'};
+    mockupTasks.push(newTask);
+    res.send('success');
+});
+    
+router.put('/editTask', (req, res) => {
+    const editedTask = req.body.task;
+    mockupTasks = mockupTasks.map(task => {
+        if (task.id === parseInt(editedTask.id.id)) {
+            return editedTask.id;
+        }
+        return task;
+    });
+    res.send('success');
+});
+    
+router.get('/', (req, res) => {
+    res.json(mockupTasks);
+});
+
+module.exports = router;
