97bfab2628c208fa33ea148250c7dc57aa4d3e19
Marta Szuran <marta.szuran@gmail.com>
2023-12-17 12:26:36 +0100

    Add jira project build

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ require('dotenv').config();
 const port = process.env.PORT;
 const router = require('./routes/tasks');
 
+const path = require("path");
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "/build");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,8 +16,17 @@ app.use(cors());
 
 app.use('/api/tasks', router);
 
-app.get('/', (req, res) => {
-  res.send('welcome')
-});
+app.use(express.static(buildPath));
+
+app.get("/*", function(req, res) {
+  res.sendFile(
+    path.join(__dirname, "/build.index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+})
 
 app.listen(port, () => console.log(`listening on port ${port}`));
